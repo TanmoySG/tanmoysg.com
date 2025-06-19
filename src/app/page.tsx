fb5ef11c99ef738c3c8d9bc9ddbde108131246ff
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2025-06-19 10:29:17 +0530

    add feedback panel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@
 import { ToggleButton } from "@/utils/buttons";
 import styles from "./page.module.css";
 import Welcome from "@/components/welcome";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import About from "@/components/about";
+import { FeedbackPopover } from "@/components/feedback/feedback";
 
 export default function Home() {
   const [showOverlay, setShowOverlay] = useState(false);
+  const [minimize, setMinimize] = useState(false);
 
   return (
     <div className={styles.page}>
@@ -16,7 +18,7 @@ export default function Home() {
         !showOverlay && (
           <ToggleButton
             buttonText="Know More"
-            onClick={() => setShowOverlay(true)}
+            onClick={() => { setShowOverlay(true); setMinimize(true); }}
             className={styles.knowMoreButton}
           />
         )
@@ -28,12 +30,16 @@ export default function Home() {
             <About className={styles.welcome} />
             <ToggleButton
               buttonText="Show Less"
-              onClick={() => setShowOverlay(false)}
+              onClick={() => {
+                setMinimize(false);
+                setShowOverlay(false);
+              }}
               className={styles.showLessButton}
             />
           </div>
         </div>
       )}
+      <FeedbackPopover minimize={minimize} />
     </div>
   );
 }
